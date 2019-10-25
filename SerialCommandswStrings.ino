288f7a732b2cb7218a4d7f5518f15b0253834116
Christos Kordalis <xristos.kordalis@gmail.com>
2019-10-25 19:47:51 +0300

    Update SerialCommandswStrings.ino

--- SerialCommandswStrings.ino
+++ SerialCommandswStrings.ino
@@ -8,18 +8,18 @@ void loop() {
   // send data only when you receive data:
   if (Serial.available() > 0) {
     // read the incoming byte:
-    String incomingByte = Serial.readString();
-    if(incomingByte == "SetLedOn")
+    String incomingString = Serial.readString();
+    if(incomingString == "SetLedOn")
       {
         digitalWrite(LED_BUILTIN, HIGH);    
         
       }
-    else if(incomingByte == "SetLedOff")
+    else if(incomingString == "SetLedOff")
       {
         digitalWrite(LED_BUILTIN, LOW);    
         
       }
-    else if(incomingByte == "ToggleLed")
+    else if(incomingString == "ToggleLed")
       {
         digitalWrite(LED_BUILTIN,  !digitalRead(LED_BUILTIN));
       }
